64e465d0058205e83d60c3bdccc165ae10a6815f
Albert Zheng <lisong.zheng@gmail.com>
2019-09-06 01:05:12 +0800

    Update foo_test.cc

--- test/foo_test.cc
+++ test/foo_test.cc
@@ -3,13 +3,13 @@
 
 #include "demo/foo.h"
 
-TEST(NemoFoo, Positives) {
+TEST(DemoFoo, Positives) {
   EXPECT_EQ(8, demo::foo(4));
   EXPECT_EQ(32, demo::foo(16));
   EXPECT_EQ(0, demo::foo(0));
 }
 
-TEST(NemoFoo, Negatives) {
+TEST(DemoFoo, Negatives) {
   EXPECT_NE(-180, demo::foo(-99));
   EXPECT_NE(0, demo::foo(-41));
   EXPECT_NE(-21, demo::foo(42));
